d6398455d2d175c931d7d375f2f7703b709f4524
rj665721-blip <rj665721@gmail.com>
2025-10-17 20:42:48 +0530

    Add files via upload

---
+++ day3q5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+    printf("%.2f°C is %.2f°F\n", celsius, fahrenheit);
+
+    return 0;
+}
