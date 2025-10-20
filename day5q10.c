8452f36f5089fffd2b43d8ba4c01092066293f41
rj665721-blip <rj665721@gmail.com>
2025-10-20 23:00:53 +0530

    Add files via upload

---
+++ day5q10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int total_seconds, hours, minutes, seconds;
+    scanf("%d", &total_seconds);
+    hours = total_seconds / 3600;
+    total_seconds %= 3600;
+    minutes = total_seconds / 60;
+    seconds = total_seconds % 60;
+    printf("%02d:%02d:%02d\n", hours, minutes, seconds);
+    return 0;
+}
