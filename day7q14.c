aa7a066d66cb3c67b11053a69cfd0b5325e85f5d
rj665721-blip <rj665721@gmail.com>
2025-10-23 23:50:45 +0530

    Add files via upload

---
+++ day7q14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf(" %c", &ch);
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+            ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            printf("Vowel\n");
+        else
+            printf("Consonant\n");
+    } else {
+        printf("Not an alphabet\n");
+    }
+    return 0;
+}
