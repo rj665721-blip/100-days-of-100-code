10c3a0ecf35379cdf8b5ae84ccc26ecc1885e1f0
rj665721-blip <rj665721@gmail.com>
2025-10-27 20:20:01 +0530

    Add files via upload

---
+++ day12q23.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int days;
+    float fine = 0;
+
+    printf("Enter number of late days: ");
+    scanf("%d", &days);
+
+    if (days <= 5)
+        fine = days * 2;
+    else if (days <= 10)
+        fine = 5 * 2 + (days - 5) * 4;
+    else if (days <= 30)
+        fine = 5 * 2 + 5 * 4 + (days - 10) * 6;
+    else {
+        printf("Membership Cancelled\n");
+        return 0;
+    }
+
+    printf("Fine = ₹%.2f\n", fine);
+    return 0;
+}
+
