47d533b4d53118c56afa847b737662d6ff631477
rj665721-blip <rj665721@gmail.com>
2025-10-29 21:44:49 +0530

    Add files via upload

---
+++ day14q27.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+        sum += 2 * i - 1;
+    printf("Sum of first %d odd numbers = %d\n", n, sum);
+    return 0;
+}
